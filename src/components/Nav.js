85839ba652e6d492a56407e5d533c67f80d68a96
promovis <promovis@gmail.com>
2020-05-02 15:08:52 +0300

    err corected

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -50,7 +50,7 @@ export class Navigation extends Component {
           </Link>
           <div className="Nav--Links">
             <NavLink to="/">Home</NavLink>
-            <NavLink to="/despre-boi/">Despre noi</NavLink>
+            <NavLink to="/despre-noi/">Despre noi</NavLink>
             <div
               className={`Nav--Group ${
                 this.state.activeSubNav === 'posts' ? 'active' : ''
