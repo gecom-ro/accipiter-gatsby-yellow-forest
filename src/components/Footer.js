d8b9883986aefdaac73181b6bce08df5b19dfe49
promovis <promovis@gmail.com>
2020-05-02 19:44:50 +0300

    upd footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,15 +6,15 @@ export default () => (
   <div>
     <h2 className="taCenter">
       Follow us{' '}
-      <a href="https://instagram.com/thrivegoldcoast/">@thrivegoldcoast</a>
+      <a href="https://instagram.com/chirana.paul/">@chirana.paul</a>
     </h2>
     <br />
     <InstagramFeed count="8" />
     <footer className="footer">
       <div className="container taCenter">
         <span>
-          © Copyright {new Date().getFullYear()} All rights reserved. Crafted by{' '}
-          <a href="https://thriveweb.com.au/">Thrive</a>.
+          © Copyright {new Date().getFullYear()} All rights reserved. Web development by{' '}
+          <a href="https://www.promovis.ro/">Promovis</a>.
         </span>
       </div>
     </footer>
